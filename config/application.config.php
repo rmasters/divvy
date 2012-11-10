71e8a63df5f83929e2c515c438cbdb618dfa6275
Ross Masters <ross@rossmasters.com>
2012-11-10 23:12:05 +0000

    Working Doctrine configuration (ORM+DBAL)

--- config/application.config.php
+++ config/application.config.php
@@ -2,6 +2,7 @@
 return array(
     'modules' => array(
         // Dependencies
+        'DoctrineModule',
         'DoctrineORMModule',
         'ZfcTwig',
         // Application
---
+++ config/autoload/orm.global.php
@@ -0,0 +1,19 @@
+<?php
+
+$entityPath = function ($module) {
+    return __DIR__ . "/../../module/$module/src/$module/Entity";
+};
+
+return array(
+    'doctrine' => array(
+        'driver' => array(
+            'orm_default' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(
+                    $entityPath('Users')
+                ),
+            ),
+        ),
+    ),
+);
