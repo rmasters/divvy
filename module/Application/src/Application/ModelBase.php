6809ea110a975bdaffe8a98ad90bcf73c3a6fe47
Ross Masters <ross@rossmasters.com>
2012-11-10 21:48:51 +0000

    Adding a base model
    This base class allows properties to be accessed via:
    
        $model->id
        $model->getId()
        $model['id']
    
    It also provides for pseudo-properties and is generally useful. See phpdoc for details.

---
+++ module/Application/src/Application/ModelBase.php
@@ -0,0 +1,209 @@
+<?php
+/**
+ * Divvy
+ *
+ * @link      https://github.com/rmasters/divvy
+ * @license   https://github.com/rmasters/divvy/blob/master/LICENSE
+ * @author    Ross Masters <ross@rossmasters.com>
+ */
+
+namespace Application;
+
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface;
+
+/**
+ * Base model
+ *
+ * Implements magic methods and properties that allow for custom getters and
+ * setters for properties without having to define both for each property.
+ *
+ * @package Models
+ */
+abstract class ModelBase implements InputFilterAwareInterface, \ArrayAccess
+{
+    /**
+     * Get the properties of the model as an array
+     * @return array
+     */
+    public function getArrayCopy() {
+        return get_object_vars($this);
+    }
+
+    /**
+     * Set properties of the model from an array
+     * @param \Traversable $data Values to use
+     * @return \Application\ModelBase $this
+     */
+    public function exchangeArray($data) {
+        foreach ($data as $key => $value) {
+            $this->__set($key, $value);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get a property
+     *
+     * This attempts to get a value in this order:
+     *   call $this->getPropertyName(), if it exists;
+     *   get $this->propertyName, if it exists;
+     *   throw an exception.
+     *
+     * The getter method should be able to be called without arguments (though
+     * it can have optional arguments for other uses).
+     * 
+     * This allows for the use of pseudo-properties - by defining a method that can
+     * then be accessed as a property ($this->getPseudo() == $this->pseudo).
+     *
+     * @param string $name Property name.
+     * @throws Exception If neither the getter method or the property exist
+     * @return mixed
+     */
+    public function __get($name) {
+        $method = 'get' . ucfirst($name);
+        if (method_exists($this, $method)) {
+            return $this->$method();
+        } else if (property_exists($this, $name)) {
+            return $this->$name;
+        } else {
+            throw new \Exception(sprintf('Cannot get invalid property %s', $name));
+        }
+    }
+
+    /**
+     * Set a property, using the same method as __get()
+     * 
+     * @param string $name Property name
+     * @param mixed $value Value to set
+     * @return void
+     */
+    public function __set($name, $value) {
+        $method = 'set' . ucfirst($name);
+        if (method_exists($this, $method)) {
+            $this->$method($value);
+            return $this;
+        } else if (property_exists($this, $name)) {
+            $this->$name = $value;
+        } else {
+            throw new \Exception(sprintf('Cannot set invalid property %s', $name));
+        }
+    }
+
+    /**
+     * Detect if a property has been set
+     * Uses the same method as __get(). Will return false if value is null.
+     *
+     * @param string $name Property name
+     * @return bool
+     */
+    public function __isset($name) {
+        return !is_null($this->__get($name));
+    }
+
+    /**
+     * Sets a property to null (using __set())
+     * @param string $name
+     * @return void
+     */
+    public function __unset($name) {
+        $this->__set($name, null);
+    }
+
+    /**
+     * Detect if a property exists in the same manner as __get() and __set()
+     * @param string $name Property name
+     * @return bool
+     */
+    public function hasProperty($name) {
+        return method_exists($this, 'get' . ucfirst($name)) || property_exists($this, $name);
+    }
+
+    /**
+     * The inverse of __get() for undefined method calls
+     *
+     * If the method is named getX or setX and has 0 or 1 arguments respectively it
+     * will return calls to __get() and __set() respectively.
+     *
+     * @param string $name Method name
+     * @param array $args Method arguments
+     * @throws Exception If no getter or setter could be detected
+     * @return mixed
+     */
+    public function __call($name, array $args = array()) {
+        // If method begins with get(.+) or set(.+)
+        if (strlen($name) > 3) {
+            // get* and set*
+            $prefix = substr($name, 0, 3);
+            if (in_array($prefix, ['get', 'set'])) {
+                $field = lcfirst(substr($name, 3));
+
+                // Pass to __set if number of args is 1
+                if ($prefix == 'set' && count($args) == 1) {
+                    call_user_func([$this, '__set'], $field, array_shift($args));
+                    return $this;
+                }
+
+                // Pass to __get if number of args is 0
+                if ($prefix == 'get' && count($args) == 0) {
+                    return call_user_func([$this, '__get'], $field);
+                }
+            }
+        }
+
+        throw new \Exception(sprintf('No method %s of class %s', $name, __CLASS__));
+    }
+
+    /**
+     * Provide access to __isset() in the array form `isset($this[$name])`
+     * @param string $name
+     * @return bool
+     */
+    public function offsetExists($name) {
+        return call_user_func([$this, '__isset'], $name);
+    }
+
+    /**
+     * Provide access to __get() in the array form `$this[$name]`
+     * @param string $name
+     * @return mixed
+     */
+    public function offsetGet($name) {
+        return call_user_func([$this, '__get'], $name);
+    }
+
+    /**
+     * Provide access to __set() in the array form `$this[$name] = $value`
+     * @param string $name
+     * @param mixed $value
+     * @return void
+     */
+    public function offsetSet($name, $value) {
+        call_user_func([$this, '__set'], $name, $value);
+    }
+
+    /**
+     * Provide access to __unset() in the array form `unset($this[$name])`
+     * @param string $name
+     * @return void
+     */
+    public function offsetUnset($name) {
+        call_user_func([$this, '__unset'], $name);
+    }
+
+    /**
+     * Prevent input filters being overridden
+     * @param \Zend\InputFilter\InputFilterInterface $if
+     * @throws Exception
+     */
+    public function setInputFilter(InputFilterInterface $if) {
+        throw new \Exception('Not used');
+    }
+
+    /**
+     * Require subclasses to define an input filter
+     * @return \Zend\InputFilter\InputFilter
+     */
+    abstract public function getInputFilter();
+}
---
+++ tests/module/Application/src/Application/ModelBaseTest.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * Divvy
+ *
+ * @link      https://github.com/rmasters/divvy
+ * @license   https://github.com/rmasters/divvy/blob/master/LICENSE
+ * @author    Ross Masters <ross@rossmasters.com>
+ */
+
+namespace Application;
+
+use Zend\InputFilter\InputFilter;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Define a simple model that extends ModelBase
+ */
+class TestModel extends ModelBase
+{
+    protected $id;
+    protected $name;
+
+    public function __construct() {
+        $this->id = 2;
+    }
+
+    public function getPseudoProperty() {
+        return '1234567890';
+    }
+
+    public function setPseudoProperty($value) {
+        $this->name = $value;
+    }
+
+    public function getInputFilter() {
+        return new InputFilter;
+    }
+}
+
+/**
+ * Tests Application\ModelBase
+ *
+ * @package Tests
+ */
+class ModelBaseTest extends PHPUnit_Framework_TestCase
+{
+    public function testPropertyAccess() {
+        $model = new TestModel;
+
+        // Test __get
+        $this->assertEquals($model->id, 2);
+
+        // Test __isset
+        $this->assertFalse(isset($model->name));
+
+        // Test __set
+        $model->name = 'Name';
+        $this->assertEquals($model->name, 'Name');
+
+        // Test __unset
+        unset($model->name);
+        $this->assertNull($model->name);
+
+        // Test getting a non-existant property
+        $this->setExpectedException('Exception');
+        $model->nonexistant;
+
+        // Test setting a non-existant property
+        $this->setExpectedException('Exception');
+        $model->nonexistant = false;
+
+        // Test getting pseduo-property via __get
+        $this->assertEquals($model->pseudoProperty, '1234567890');
+
+        // Test setting a psuedo-property via __set
+        $model->pseudoProperty = 'example';
+        $this->assertEquals($model->name, 'example');
+    }
+
+    public function testMethodAccess() {
+        $model = new TestModel;
+
+        // Test get via __call
+        $this->assertEquals($model->getId(), 2);
+
+        // Test __set
+        $model->setName('Name');
+        $this->assertEquals($model->getName(), 'Name');
+
+        // Test getting a non-existant property
+        $this->setExpectedException('Exception');
+        $model->getNonexistant();
+
+        // Test setting a non-existant property
+        $this->setExpectedException('Exception');
+        $model->setNonexistant(false);
+
+        // Test getting pseduo-property via __get
+        $this->assertEquals($model->getPseudoProperty(), '1234567890');
+
+        // Test setting a psuedo-property via __set
+        $model->setPseudoProperty('example');
+        $this->assertEquals($model->getName(), 'example');
+    }
+
+    public function testArrayAccess() {
+        $model = new TestModel;
+
+        // Test offsetGet
+        $this->assertEquals($model['id'], 2);
+
+        // Test offsetExists
+        $this->assertFalse(isset($model['name']));
+
+        // Test offsetSet
+        $model->name = 'Name';
+        $this->assertEquals($model['name'], 'Name');
+
+        // Test offsetUnset
+        unset($model['name']);
+        $this->assertNull($model['name']);
+
+        // Test getting a non-existant property
+        $this->setExpectedException('Exception');
+        $model['nonexistant'];
+
+        // Test setting a non-existant property
+        $this->setExpectedException('Exception');
+        $model['nonexistant'] = false;
+
+        // Test getting pseduo-property via __get
+        $this->assertEquals($model['pseudoProperty'], '1234567890');
+
+        // Test setting a psuedo-property via __set
+        $model['pseudoProperty'] = 'example';
+        $this->assertEquals($model['name'], 'example');
+    }
+
+    public function testExchangeArray() {
+        $model = new TestModel;
+
+        $ret = $model->exchangeArray(['id' => 5, 'name' => 'Dawleys']);
+
+        $this->assertSame($ret, $model);
+        $this->assertEquals($model->id, 5);
+        $this->assertEquals($model->name, 'Dawleys');
+
+        $this->setExpectedException('Exception');
+        $model->exchangeArray(['fake' => 123]);
+    }
+
+    public function testGetArrayCopy() {
+        $model = new TestModel;
+
+        $this->assertEquals($model->getArrayCopy(), ['id' => 2, 'name' => null]);
+    }
+
+    public function testInputFilter() {
+        $model = new TestModel;
+
+        $this->setExpectedException('Exception');
+        $model->setInputFilter(new InputFilter);
+    }
+}
